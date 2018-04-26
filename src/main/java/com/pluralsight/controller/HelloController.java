e215591d7157033c6624fa827e2e433e34e60cd3
William Zhou <williamkzhou@hotmail.com>
2018-04-26 16:29:10 +0100

    Updated package files

--- src/main/java/com/pluralsight/controller/HelloController.java
+++ src/main/java/com/pluralsight/controller/HelloController.java
@@ -6,12 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	
-	@RequestMapping(value = "/greeting")
-	public String sayHello(Model model) {
+
+	@RequestMapping(value ="/greeting")
+	public String sayHello (Model model) {
 		
-		model.addAttribute("greeting", "Hello World!");
+		model.addAttribute("greeting", "Hello World");
 		
 		return "hello";
 	}
+	
 }
